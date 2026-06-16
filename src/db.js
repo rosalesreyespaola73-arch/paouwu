9a822f484c13014593666b53e8220f476c03d9e4
Paola Rosales <rosalesreyespaola73@gmail.com>
2026-06-16 02:44:52 -0500

    Agregar SSL para Aiven

--- src/db.js
+++ src/db.js
@@ -1,16 +1,17 @@
 import mysql from 'mysql2/promise';
 import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } from './config.js';
 
-const poolConfig = {
+const pool = mysql.createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_DATABASE,
-    port: DB_PORT
-};
+    port: DB_PORT,
 
-const pool = mysql.createPool({
-    ...poolConfig,
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
